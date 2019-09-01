d482179402fb1f90ebdf3189bcbf581e856b651f
Alex <alnoutash@hotmail.com>
2019-09-01 16:47:05 -0400

    wip

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,8 +1,6 @@
-import {Component, OnInit} from '@angular/core';
+import { Component, ViewChild, OnInit } from '@angular/core';
 import {Transport} from '../transport/transport.service';
-import {Platform} from '@ionic/angular';
-import {SplashScreen} from '@ionic-native/splash-screen/ngx';
-import {StatusBar} from '@ionic-native/status-bar/ngx';
+import { IonInfiniteScroll } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -10,11 +8,43 @@ import {StatusBar} from '@ionic-native/status-bar/ngx';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage implements OnInit {
+
   constructor(
-      private transport: Transport){}
+      private transport: Transport) {
+    this.dataList = [];
+
+    for (let i = 0; i < 10; i++) {
+      this.dataList.push('Item number ' + this.dataList.length);
+    }
+  }
   postsCollection: any;
+  dataList: any;
+  @ViewChild(IonInfiniteScroll) infiniteScroll: IonInfiniteScroll;
+
   ngOnInit() {
     this.transport.getPosts().subscribe(data => {
       this.postsCollection = data;  });
+  }
+
+
+  loadData(event) {
+
+    setTimeout(() => {
+      console.log('Done');
+      for (let i = 0; i < 10; i++) {
+        this.dataList.push('Item number ' + this.dataList.length);
+      }
+      event.target.complete();
+
+      // App logic to determine if all data is loaded
+      // and disable the infinite scroll
+      if (this.dataList.length === 100) {
+        event.target.disabled = true;
+      }
+    }, 500);
+  }
+
+  toggleInfiniteScroll() {
+    this.infiniteScroll.disabled = !this.infiniteScroll.disabled;
   }
 }
