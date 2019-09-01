c48ad0d71926c4bab3c2f748e7e25db1a7258e53
Alex <alnoutash@hotmail.com>
2019-09-01 16:08:02 -0400

    wip

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -6,6 +6,7 @@ import { RouterModule } from '@angular/router';
 import {Transport} from '../transport/transport.service';
 import { ScrollingModule } from '@angular/cdk/scrolling';
 import { HomePage } from './home.page';
+import {MaterialModule} from '../material.module';
 
 @NgModule({
   imports: [
@@ -13,6 +14,7 @@ import { HomePage } from './home.page';
     FormsModule,
     IonicModule,
     ScrollingModule,
+      MaterialModule,
     RouterModule.forChild([
       {
         path: '',
